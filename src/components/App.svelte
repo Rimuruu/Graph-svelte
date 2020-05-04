6550b7e18eca802783fc58135e88630e5063aa90
Rimuruu <antoine.renciot@gmail.com>
2020-05-04 17:39:24 +0200

    deploy

---
+++ src/components/App.svelte
@@ -0,0 +1,148 @@
+<script>
+import Chart from 'chart.js';
+import { onMount } from 'svelte';
+
+import {tableau} from '../store/tableau.js';
+
+const MONTH = ['Janvier','Fevrier','Mars','Avril','Mai','Juin','Juillet','Aout','Septembre','Octobre','Novembre','Decembre']
+
+var ctx;
+var myChart;
+var count = 0;
+var nom=''
+var varY = [0,0,0,0,0,0,0,0,0,0,0,0];
+
+
+onMount(() => {
+myChart = new Chart(ctx.getContext('2d'), {
+    type: 'line',
+    data: {
+        labels: MONTH,
+        datasets: []
+	},
+	
+    options: {
+		responsive: true,
+		maintainAspectRatio: false,
+        scales: {
+            yAxes: [{
+                ticks: {
+					beginAtZero: true,
+					fontSize: 20,
+                }
+			}],
+			xAxes: [{
+                ticks: {
+					beginAtZero: false,
+					fontSize: 25,
+                }
+            }]
+		},
+		
+	}
+	});
+})
+
+function addValue(){
+	if(nom != ''){
+		count++;
+		let color = [Math.floor(Math.random() * 255),Math.floor(Math.random() * 255),Math.floor(Math.random() * 255)];
+		let data = {
+				label: varY[0],
+				data: varY,
+				backgroundColor: 
+					'rgba('+color[0]+', '+color[1]+', '+color[2]+', 0.2)',
+			
+		
+				borderColor: 
+					'rgba('+color[0]+', '+color[1]+', '+color[2]+', 1)',
+				
+				borderWidth: 1
+			}
+		$tableau = [...$tableau,data];
+		myChart.data.datasets.push(data);
+		myChart.update();
+	}
+	else{
+		alert('Veuillez inscrire un nom');
+	}
+}
+
+
+function addRandom(){
+	count++;
+	let color = [Math.floor(Math.random() * 255),Math.floor(Math.random() * 255),Math.floor(Math.random() * 255)];
+	let data = {
+            label: 'Random '+count,
+            data: [
+				Math.floor(Math.random() * 10),
+				Math.floor(Math.random() * 10) ,
+				Math.floor(Math.random() * 10) ,
+				Math.floor(Math.random() * 10) ,
+				Math.floor(Math.random() * 10) ,
+				Math.floor(Math.random() * 10) ,
+				Math.floor(Math.random() * 10) ,
+				Math.floor(Math.random() * 10) ,
+				Math.floor(Math.random() * 10) ,
+				Math.floor(Math.random() * 10) ,
+				Math.floor(Math.random() * 10) ,
+				Math.floor(Math.random() * 10)  ],
+            backgroundColor: 
+                'rgba('+color[0]+', '+color[1]+', '+color[2]+', 0.2)',
+        
+      
+            borderColor: 
+                'rgba('+color[0]+', '+color[1]+', '+color[2]+', 1)',
+			
+            borderWidth: 1
+		};
+	$tableau = [...$tableau,data];
+	myChart.data.datasets.push(data);
+	myChart.update();
+}
+
+</script>
+
+<div id="container">
+	<canvas bind:this={ctx} id="myChart"></canvas>
+	<div>
+		<input bind:value={nom} required placeholder="Nom" size="4">
+		<input bind:value={varY[0]}  type="number" placeholder="Janvier" >
+		<input bind:value={varY[1]}  type="number" placeholder="Fevrier" >
+		<input bind:value={varY[2]}  type="number" placeholder="Mars" >
+		<input bind:value={varY[3]}  type="number" placeholder="Avril" >
+		<input bind:value={varY[4]}  type="number" placeholder="Mai" >
+		<input bind:value={varY[5]}  type="number" placeholder="Juin" >
+		<input bind:value={varY[6]}  type="number" placeholder="Juillet" >
+		<input bind:value={varY[7]}  type="number" placeholder="Aout" >
+		<input bind:value={varY[8]}  type="number" placeholder="Septembre" >
+		<input bind:value={varY[9]}  type="number" placeholder="Octobre" >
+		<input bind:value={varY[10]} type="number" placeholder="Novembre" >
+		<input bind:value={varY[11]}  type="number" placeholder="Decembre" >
+		<button on:click={addValue}>Ajouter</button>
+	</div>
+	<button on:click={addRandom}>Ajouter Random</button>
+
+</div>
+
+<style>
+
+#container{
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	width: 100%;
+	height: 100%;
+	
+}
+
+canvas{
+	
+	width: 900px !important;
+	height: 600px !important;
+}
+
+input {
+  width: 110px;
+}
+</style>
---
+++ src/components/GraphList.svelte
@@ -0,0 +1,24 @@
+
+<script>
+import {tableau} from '../store/tableau.js';
+import { fly } from 'svelte/transition';
+$: tab = $tableau;
+
+</script>
+<div transition:fly={{x: 250, opacity: 1}} id='list'>
+    <ul>
+    {#each tab as item}
+    <li>{item.label}</li>
+    {/each}
+    </ul>
+</div>
+
+<style>
+#list{
+    background-color: aliceblue;
+    width:150px;
+    right: 0;
+    position: absolute;
+}
+
+</style>
